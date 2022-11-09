91c3fdec1cc484ba0f32037afe88d8501e81d001
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-09 16:35:49 +0600

    signup design done.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css'
 const Login = () => {
     return (
-        <div>
-           Login 
+        <div className='container mw-50 py-4 form-section'>
+            <Form className='form mw-50 h-100 mx-auto' >
+                <h2 className='text-center pb-3'>Login</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button className='btn w-100 bg-black border-dark'  type="submit">
+                   Login
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Signup = () => {
     return (
-        <div>
-            Signup.
+        <div className='container mw-50 py-4 form-section'>
+            <Form className='form mw-50 h-100 mx-auto' >
+                <h2 className='text-center pb-3'>Signup</h2>
+
+                <Form.Group className="mb-3" controlId="formBasicname">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPhoto">
+                    <Form.Label>Photo</Form.Label>
+                    <Form.Control type="text" placeholder="Photo url" />
+                </Form.Group>
+                <Button className='btn w-100 bg-black border-dark'  type="submit">
+                    Signup
+                </Button>
+            </Form>
         </div>
     );
 };
