09d8b926b574eba8c9b4e0a9bcb7284cdb63ad23
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-09 22:42:19 +0600

    spinner added.

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../customContexts/AuthProvider';
-
-
 const PrivateRoute = ({ children }) => {
-    
+
     const location = useLocation();
-    const { user,loading } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     if (loading) {
-        return <div>loading....</div>
+        return <> <Spinner animation="border" />
+            <div>loading....</div> </>
     }
     if (!user) {
-        return <Navigate to='/login' state={{form:location}} replace></Navigate>;
+        return <Navigate to='/login' state={{ form: location }} replace></Navigate>;
     }
     return children;
-    
+
 };
 
 export default PrivateRoute;
