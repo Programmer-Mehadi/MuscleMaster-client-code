c8c5938a501c44157139a9ea8cbf3a6c4e858a25
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-09 20:53:13 +0600

    service card redesign..

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
-import './ServiceCard.css'
+
+import './ServiceCard.css';
 const ServiceCard = ({ service }) => {
-    const {_id,image,serviceName,rating,price,description } = service;
+    const { _id, image, serviceName, rating, price, description } = service;
     return (
-        <Col>
-            <Card style={{ marginBottom: '10px',minHeight:'450px' }}>
-                <Card.Img variant="top" className='p-3 image' style={{height:'200px',borderRadius:'5px !important'}} src={image} />
+        <Col >
+            <Card className="shadow" style={{ marginBottom: '10px', minHeight: '450px' }}>
+                <Card.Img variant="top" className='p-3 image' style={{ height: '200px', borderRadius: '5px !important' }} src={image} />
                 <Card.Body>
                     <Card.Title>{serviceName}</Card.Title>
                     <Card.Text>
-                         {description.length > 100 ? `${description.slice(0,100)}...`: description}
+                        {description.length > 100 ? `${description.slice(0, 100)}...` : description}
                     </Card.Text>
-                    <Card.Text>Rating: {rating}
-                    </Card.Text>
-                    <Card.Text>Price: {price}
-                    </Card.Text>
-                    <Button variant="primary"> <Link style={{color:'white',textDecoration:'none'}} to={`/services/${_id}`}>View details</Link> </Button>
+                    <Table striped bordered rounded hover>
+                        <thead>
+                            <tr>
+                                <th className='text-center'>{price}<br/>Price</th>
+                                <th className='text-center'>{rating}<br/>Rating</th>
+                            </tr>
+                        </thead>
+                     
+                    </Table>
+
+                    <Button className='w-100 rounded' variant="primary"> <Link style={{ color: 'white', textDecoration: 'none' }} to={`/services/${_id}`}>View details</Link> </Button>
                 </Card.Body>
             </Card>
-            </Col>
+        </Col>
     );
 };
 
