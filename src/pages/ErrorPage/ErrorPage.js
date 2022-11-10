44c10460280e7c15e57e1a05253d3060f8ae4219
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-11-10 19:51:25 +0600

    404 page complete.

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import logo from '../../404.jpg'
 const ErrorPage = () => {
     return (
         <div>
-            Error.<br></br>
-            <Link to='/'>Home</Link>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+                <img style={{ maxWidth: '700px' }} className='w-75  mx-auto' src={logo} alt="" />
+          
+                <Link to='/'><button className="btn btn-primary">Home</button></Link>
+              
+            </div>
+            
+            
         </div>
     );
 };
